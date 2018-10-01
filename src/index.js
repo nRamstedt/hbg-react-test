0f6f5cc220536decea09b30d44b570e7031e1982
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-10-01 16:47:06 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+    Button: require('./Button/Button.jsx'),
+    Dropdown: require('./Dropdown/Dropdown.jsx'),
+    DropdownItem: require('./Dropdown/DropdownItem.jsx'),
+    DropdownList: require('./Dropdown/DropdownList.jsx'),
+    DropdownToggle: require('./Dropdown/DropdownToggle.jsx')
+}
